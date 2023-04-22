04c39d2bd676edafd7f3c9c85c2c97a587b8bb24
angelinetipa <angelinetipa@gmail.com>
2023-04-22 23:31:38 +0800

    test

--- Extract Even Odd Numbers.py
+++ Extract Even Odd Numbers.py
@@ -1,10 +1,13 @@
 # Read numbers.txt and Create even.txt and odd.txt
-
+with open("numbers.txt", "r") as file_int, open("even.txt", "w") as file_even, open("odd.txt", "w") as file_odd:
 # for each line in file_int
-
+    for line in file_int:
+        integer = int(line)
     #if even, write to even.txt
-
+        if integer % 2 == 0:
+            file_even.write(str(integer))
     #if odd, write to odd.txt
-
+        else:
+            file_odd.write(str(integer))
 # print output
  
